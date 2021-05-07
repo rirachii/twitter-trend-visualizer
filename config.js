037950008b11f604001ccb4ebd8c5f044725072a
gulso3387 <70995875+gulso3387@users.noreply.github.com>
2021-05-07 17:25:04 -0400

    commit on main

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ module.exports={
     // access_token: process.env.Access_Token,
     // access_token_secret: process.env.Access_Token_Secret,
 
-    // consumer_key:"j0PHMzf1RoaIkt9iLYoewRZ7A",
-    // consumer_secret: "mh6zSlBQJ5JP1bV0WRBf73sFjjsRTP3CtL96Vo3gQDpQF3hZKx",
-    // access_token: "4320535713-riDUVgR7naT9uTkun1JGckApDEhE0lwpTIM4b7Z",
-    // access_token_secret: "5d0zh4PNYQSFRutqoE5bRmI7PGOVwQzwJbHjYmjuAxLMm",
+    consumer_key:"j0PHMzf1RoaIkt9iLYoewRZ7A",
+    consumer_secret: "mh6zSlBQJ5JP1bV0WRBf73sFjjsRTP3CtL96Vo3gQDpQF3hZKx",
+    access_token: "4320535713-riDUVgR7naT9uTkun1JGckApDEhE0lwpTIM4b7Z",
+    access_token_secret: "5d0zh4PNYQSFRutqoE5bRmI7PGOVwQzwJbHjYmjuAxLMm",
 
-    consumer_key: 'jJKwYVKkqnSkbr63NpK7Vzvkx',
-    consumer_secret: 'lTMfwMs7rDxz8vPcxBD7Gy5lDO8GXnlIzPw2d8xVhqa1L4xOTw',
-    access_token:'364475473-kMBumzdzoxKZcduTwFGizG0iyMldRx1CQtcRXm2w',
-    access_token_secret: 'KBjBbqUZ0of2SQZDFEqSFof7kQPpENigIh7d3BMUQyCjN',
+    // consumer_key: 'jJKwYVKkqnSkbr63NpK7Vzvkx',
+    // consumer_secret: 'lTMfwMs7rDxz8vPcxBD7Gy5lDO8GXnlIzPw2d8xVhqa1L4xOTw',
+    // access_token:'364475473-kMBumzdzoxKZcduTwFGizG0iyMldRx1CQtcRXm2w',
+    // access_token_secret: 'KBjBbqUZ0of2SQZDFEqSFof7kQPpENigIh7d3BMUQyCjN',
 }
