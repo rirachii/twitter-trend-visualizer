96821fb5f13b8a1f1d33212177105b6436a9792d
Mic <82119639+rirachii@users.noreply.github.com>
2021-05-29 10:07:48 -0400

    Update Config.js
    Removed keys

--- config.js
+++ config.js
@@ -3,12 +3,7 @@ module.exports={
     // consumer_key: process.env.Consumer_Key,
     // consumer_secret: process.env.Consumer_Secret,
     // access_token: process.env.Access_Token,
-    // access_token_secret: process.env.Access_Token_Secret,
-
-    // consumer_key:"j0PHMzf1RoaIkt9iLYoewRZ7A",
-    // consumer_secret: "mh6zSlBQJ5JP1bV0WRBf73sFjjsRTP3CtL96Vo3gQDpQF3hZKx",
-    // access_token: "4320535713-riDUVgR7naT9uTkun1JGckApDEhE0lwpTIM4b7Z",
-    // access_token_secret: "5d0zh4PNYQSFRutqoE5bRmI7PGOVwQzwJbHjYmjuAxLMm",
+    // access_token_secret: process.env.Access_Token_Secret
 
     consumer_key: 'jJKwYVKkqnSkbr63NpK7Vzvkx',
     consumer_secret: 'lTMfwMs7rDxz8vPcxBD7Gy5lDO8GXnlIzPw2d8xVhqa1L4xOTw',
